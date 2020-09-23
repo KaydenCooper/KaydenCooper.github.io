7e7177c7ecd34506986d7381cf7558aad4d47e69
Kayden Cooper <kayden_cooper@yahoo.com>
2020-09-23 11:25:39 -0600

    build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="home row container-fluid"></div>
+  <div class="home row container-fluid mx-0 px-0">
+    <div class="carousel slide carousel-fade" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-interval="5000">
+          <img src="../assets/DSC00559-2.jpg" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item" data-interval="5000">
+          <img src="../assets/DSC01239.jpg" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item" data-interval="5000">
+          <img src="../assets/DSC00585-2.jpg" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item" data-interval="5000">
+          <img src="../assets/DSC01251.jpg" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item" data-interval="5000">
+          <img src="../assets/DSC00587-2.jpg" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item" data-interval="5000">
+          <img src="../assets/DSC00750-2.jpg" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item" data-interval="5000">
+          <img src="../assets/DSC00450-2.jpg" class="d-block w-100" alt="..." />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,6 +40,32 @@ export default {
 </script>
 
 <style scoped>
+.carousel-fade,
+.carousel-inner,
+.item {
+  transition-property: opacity;
+}
+
+.item,
+.active.left,
+.active.right {
+  opacity: 0;
+}
+
+.active,
+.next.left,
+.prev.right {
+  opacity: 1;
+}
+
+.next,
+.prev,
+.active.left,
+.active.right {
+  left: 0;
+  transform: translate3d(0, 0, 0);
+}
+
 .height {
   height: 90vh;
 }
